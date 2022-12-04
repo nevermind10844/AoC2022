80e2515acb1b1bb24343f67584f70698462c785f
Jan Kressin <jan.kressin@googlemail.com>
2022-12-04 10:03:23 +0100

    Day1_1 solved

---
+++ AoC_Day4_1/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<String> strings = LineReader.read();
+		int sum = 0;
+		for(String s : strings) {
+			System.out.println(s);
+			String[] split = s.split(",");
+			String[] left = split[0].split("-");
+			
+			List<Integer> leftRange = new ArrayList<>();
+			for(int i = Integer.valueOf(left[0]); i<Integer.valueOf(left[1])+1; i++) {
+				leftRange.add(i);
+			}
+			
+			String[] right = split[1].split("-");
+
+			List<Integer> rightRange = new ArrayList<>();
+			
+			for(int i = Integer.valueOf(right[0]); i<Integer.valueOf(right[1])+1; i++) {
+				rightRange.add(i);
+			}
+			
+			if(rightRange.containsAll(leftRange) || leftRange.containsAll(rightRange))
+				sum++;
+			
+			System.out.println(sum); //571
+		}
+	}
+
+}

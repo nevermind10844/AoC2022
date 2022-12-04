a18d9e18346457e74e2020157ad4b7915a532793
Jan Kressin <jan.kressin@googlemail.com>
2022-12-04 15:17:47 +0100

    Made Day for a bit nicer

--- AoC_Day4_1/src/Main.java
+++ AoC_Day4_1/src/Main.java
@@ -1,38 +1,30 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> strings = LineReader.read();
 		int sum = 0;
-		for(String s : strings) {
-			System.out.println(s);
+		for (String s : strings) {
 			String[] split = s.split(",");
-			String[] left = split[0].split("-");
-			
-			List<Integer> leftRange = new ArrayList<>();
-			for(int i = Integer.valueOf(left[0]); i<Integer.valueOf(left[1])+1; i++) {
-				leftRange.add(i);
-			}
-			
-			String[] right = split[1].split("-");
 
-			List<Integer> rightRange = new ArrayList<>();
+			String[] left = split[0].split("-");
+			List<Integer> leftRange = IntStream.range(Integer.valueOf(left[0]), Integer.valueOf(left[1]) + 1).boxed()
+					.toList();
+
+			String[] right = split[1].split("-");
+			List<Integer> rightRange = IntStream.range(Integer.valueOf(right[0]), Integer.valueOf(right[1]) + 1).boxed()
+					.toList();
 			
-			for(int i = Integer.valueOf(right[0]); i<Integer.valueOf(right[1])+1; i++) {
-				rightRange.add(i);
-			}
-			
-			for(int part : leftRange) {
-				if(rightRange.contains(part)) {
+			for (int part : leftRange) {
+				if (rightRange.contains(part)) {
 					sum++;
 					break;
 				}
 			}
-			
 		}
-		System.out.println(sum); //917
+		System.out.println(sum); // 917
 	}
 
 }

f99fdeca5a4fdbcb083975e7ae2f93c57f5d2b11
Jan Kressin <jan.kressin@googlemail.com>
2022-12-13 21:24:21 +0100

    implementing pair clss in common lib

---
+++ AOC_commonLibrary/src/fun/learn/aoc/Pair.java
@@ -0,0 +1,20 @@
+package fun.learn.aoc;
+
+public class Pair<T1, T2> {
+	private T1 left;
+	private T2 right;
+	
+	public Pair(T1 left, T2 right) {
+		super();
+		this.left = left;
+		this.right = right;
+	}
+	
+	public T1 getLeft() {
+		return left;
+	}
+	
+	public T2 getRight() {
+		return right;
+	}
+}
